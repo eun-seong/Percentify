5bec8b864cb98de7081242fc3d73a678f7985b13
eun-seong <eunseongofficial@gmail.com>
2020-03-05 10:12:03 +0900

    first store

--- app/src/main/java/com/eun/percentify/CountWidgetConfigureActivity.java
+++ app/src/main/java/com/eun/percentify/CountWidgetConfigureActivity.java
@@ -19,6 +19,11 @@ import android.widget.Toast;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -33,8 +38,11 @@ public class CountWidgetConfigureActivity extends Activity implements RecyclerAd
     private Context mContext;
     private RemoteViews views;
     private AppWidgetManager appWidgetManager;
+    private AdView mAdView;
 
     private EditText title, start_input, unit, current_input, finish_input;
+    private double start = -1, finish= -1, current =-1;
+    private int percent= -1;
 
     // RecyclerView
     private int mPosition;
@@ -64,11 +72,58 @@ public class CountWidgetConfigureActivity extends Activity implements RecyclerAd
         appWidgetManager = AppWidgetManager.getInstance(this);
         views = new RemoteViews(this.getPackageName(), R.layout.count_widget);
 
+
+
+        MobileAds.initialize(this, getString(R.string.admob_app_id));
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        // 광고가 제대로 로드 되는지 테스트 하기 위한 코드입니다.
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                // 광고가 문제 없이 로드시 출력됩니다.
+                Log.d(TAG, "onAdLoaded");
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+                // 광고 로드에 문제가 있을시 출력됩니다.
+                Log.d(TAG, "onAdFailedToLoad " + errorCode);
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+
+            @Override
+            public void onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+        });
         /************ 버튼 ************/
         title = findViewById(R.id.title_input);
         start_input = findViewById(R.id.start_input);
         current_input = findViewById(R.id.current_input);
         finish_input = findViewById(R.id.finish_input);
+
         unit = findViewById(R.id.unit);
         unit.addTextChangedListener(new TextWatcher() {
             @Override
@@ -86,7 +141,6 @@ public class CountWidgetConfigureActivity extends Activity implements RecyclerAd
 
             @Override
             public void afterTextChanged(Editable s) {
-
             }
         });
 
@@ -141,7 +195,6 @@ public class CountWidgetConfigureActivity extends Activity implements RecyclerAd
 
         setRecyclerView();
     }
-
 
     /******************** RecyclerView ************************/
     private void setRecyclerView() {
